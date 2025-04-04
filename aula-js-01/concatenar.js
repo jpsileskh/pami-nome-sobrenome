f69e2379cbfe465b0aca37273b3a43b13d22d8ae
João Siles <jpisileskh@hotmail.com>
2025-04-03 22:30:34 -0300

    Aula de javascript

---
+++ aula-js-01/concatenar.js
@@ -0,0 +1,11 @@
+let palavra01 = "Quero";
+let palavra02 = "um";
+let palavra03 = "Switch 2!";
+
+console.log(palavra01, palavra02, palavra03);
+console.log(palavra01 + palavra02 + palavra03);
+console.log(`${palavra01} ${palavra02} ${palavra03}♥ 
+            \n ♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥`);
+console.log(palavra01, "um" , palavra03);
+console.log(palavra01 + " um " + palavra03);
+console.log(`${palavra01} um ${palavra03}`);
---
+++ aula-js-01/ola.js
@@ -0,0 +1,16 @@
+console.log("Olá mundo!");
+
+var msg = "Eu quero um Nintendo Switch 2!";
+console.log(msg);
+
+let resposta = "meus alunos são lindos! #SW2";
+console.log(resposta);
+
+const turma = "eu ♥ o 2DS";
+console.log(turma);
+
+resposta = "me comprem o switch 2 pfvr?";
+console.log(resposta);
+
+// turma = "Teste";
+// console.log(turma);
